9ba76943e7db0ed412fa17adecf6392833bc875a
Yash Vardhan Singh <singhyashvardhan34@gmail.com>
2025-08-27 00:51:02 +0530

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,38 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,jsx,ts,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,jsx,ts,tsx}",
+    
+//   ],
+//   theme: {
+//     extend: {
+//       container: { center: true, padding: "1rem" },
+//       colors: {
+//         brand: {
+//           50: "#ecfeff",
+//           100: "#cffafe",
+//           500: "#06b6d4",
+//           600: "#0891b2",
+//           700: "#0e7490"
+//         }
+//       }
+//     },
+//   },
+//   plugins: [],
+// };
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      container: { center: true, padding: "1rem" },
-      colors: {
-        brand: {
-          50: "#ecfeff",
-          100: "#cffafe",
-          500: "#06b6d4",
-          600: "#0891b2",
-          700: "#0e7490"
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
